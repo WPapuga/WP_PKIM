b9c5f58598646e234a7700b63d28e18ccf4c14db
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-05 01:45:01 +0200

    update Footer

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='Footer'>
-        <p>Footer</p>
+        <p>Źródła</p>
         <a href="https://www.flaticon.com/free-icons/advertising" title="advertising icons">Advertising icons created by DinosoftLabs - Flaticon</a>
         <br/>
         <a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by Kiranshastry - Flaticon</a>   
