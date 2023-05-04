3c03f786c547f8095dbdefbb5fb6742b5f8f38b5
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-04 18:29:18 +0200

    update App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,28 @@ import Ads from './pages/Ads'
 import Currencies from './pages/Currencies'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import PostAd from './pages/PostAd'
+import MyAds from './pages/MyAds'
+import LogOut from './pages/LogOut'
+import Footer from './Components/Footer'
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Navbar/>
     <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/ogloszenia" element={ <Ads/> } />
         <Route path="/waluty" element={ <Currencies/> } />
         <Route path="/logowanie" element={ <SignIn/> } />
         <Route path="/rejestracja" element={ <SignUp/> } />
+        <Route path="/tworzenie" element={ <PostAd/> } />
+        <Route path="/mojeOgloszenia" element={ <MyAds/> } />
+        <Route path="/wylogowanie" element={ <LogOut/> } />
     </Routes>
+    <Footer/>
     </>
   );
 }
