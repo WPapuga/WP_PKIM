025197cfc64ed5b3334dc3ade65b6cf6b717e146
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-04 18:28:33 +0200

    add Footer

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='Footer'>
+        <p>Footer</p>
+        <a href="https://www.flaticon.com/free-icons/advertising" title="advertising icons">Advertising icons created by DinosoftLabs - Flaticon</a>   
+    </div>
+  )
+}
+
+export default Footer
