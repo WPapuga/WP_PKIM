6351b0d2b9599eecea059c4a0d7a83fe5e6d757c
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-04 02:59:03 +0200

    add connection between server and db

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mysql = require('mysql2');
 const app = express()
 const port = 3080
+
+const conn = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: '123',
+    database: 'wp_pkim_db',
+});
+
+conn.connect(function(err) {
+    if (err) {
+      console.error('Database connection error ' + err.stack);
+      return;
+    }
+    console.log('Connection has been established on thread: ' + conn.threadId);
+  });
+  
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,10 +25,24 @@ app.use((req, res, next) => {
   });
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+    res.send('Hello world!')
 })
 app.get('/test', (req, res) => {
-  res.send('test')
+    res.send('test')
+});
+app.get('/users', (req, res) => {
+    conn.query('SELECT * FROM users', function (error, results, fields) {
+    if (error) throw error;
+    console.log('Rekordy z tabeli users: ', results);
+    res.send(results);
+    });
+});
+app.get('/adverts', (req, res) => {
+    conn.query('SELECT adverts.id, price, content, username FROM adverts join users on adverts.user_id = users.id', function (error, results, fields) {
+      if (error) throw error;
+      console.log('Rekordy z tabeli adverts: ', results);
+      res.send(results);
+    });
 });
 
 app.listen(port, () => {
