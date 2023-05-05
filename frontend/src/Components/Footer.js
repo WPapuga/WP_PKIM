59f07e0937338e96e4c65ef38c373a911b8edb12
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-05 02:44:17 +0200

    add new icons

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -8,6 +8,10 @@ function Footer() {
         <a href="https://www.flaticon.com/free-icons/advertising" title="advertising icons">Advertising icons created by DinosoftLabs - Flaticon</a>
         <br/>
         <a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by Kiranshastry - Flaticon</a>   
+        <br/>
+        <a href="https://www.flaticon.com/free-icons/pound" title="pound icons">Pound icons created by Freepik - Flaticon</a>
+        <br />
+        <a href="https://www.flaticon.com/free-icons/trade" title="trade icons">Trade icons created by Smashicons - Flaticon</a>
     </div>
   )
 }
